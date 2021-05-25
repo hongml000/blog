23413dfafecd71aa3c6d6dc3ff47af2bc79c451d
hongml000 <286995954@qq.com>
2021-05-25 18:05:10 +0800

    init my blog

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # 仓库说明
 展示个人博客，记录日常学习内容
 
+
+## 目录说明
+- .vuepress 总配置文件
+- issuesRemark 问题记录
+- about.md 关于我
+
+
 ## 启动
-npm run dev
+安装依赖：`npm i `
+启动项目：`npm run dev`
+
+## 打包
+`npm run build`
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,14 @@
+const moment = require('moment');
+moment.locale('zh-cn') // 默认是中文
+
 module.exports = {
   title: "老喵的学习笔记", // 标题
   description: "爱生活，爱记录", // 描述
   dest: "./dist", // 打包输出位置
   port: "7777", // 启动服务的端口号
   head: [
-    // 改下图标
-    ["link", { rel: "icon", href: "/home.jpeg" }],
+    // 改下标题图标，默认资源路径是 docs/.vuepress/public/
+    ["link", { rel: "icon", href: "/cat1.jpeg" }],
     // SEO
     ["meta", {name: 'author', content: '老喵'}],
     ["meta", {name: 'keyword', content: 'vuepress 老喵的学习笔记'}]
@@ -14,15 +17,26 @@ module.exports = {
   markdown: {
     lineNumbers: true, // markdown中是否显示行号
   },
+  plugins: [
+    // 配置@vuepress/last-updated插件，自定义日期格式
+    [
+      '@vuepress/last-updated',
+      {
+        transformer: (timestamp, lang) => {
+          return moment(timestamp).format('YYYY/MM/DD HH:MM:SS')
+        }
+      }
+    ]
+  ],
   themeConfig: {
     // nav是导航栏内容
     nav: require("./nav"),
     // 侧边栏
     sidebar: require("./sidebar"),
-
     sidebarDepth: 2,  // 侧边栏的层级数
-    // 最后更新时间，设置成“Last Updated”将获取每个文件最后一次 git 提交的 UNIX 时间戳(ms)，同时它将以合适的日期格式显示在每一页的底部
-    lastUpdated: "Last Updated",
+    // 基于git的最后更新时间，将获取每个文件最后一次 git 提交的 UNIX 时间戳(ms)，同时它将以合适的日期格式显示在每一页的底部
+    // 显示效果为： 最后更新: 5/25/2021, 4:50:23 PM
+    lastUpdated: "最后更新",
     searchMaxSuggestoins: 10,
     serviceWorker: {
       updatePopup: {
